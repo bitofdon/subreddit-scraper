402d9dfa9dfd6f3a7a6d4cedfb39535c20a5950a
bitofdon <bitdon@protonmail.com>
2019-04-22 14:42:04 -0500

    getting a specific subreddit page, beautifulsoup object def

--- scraper.py
+++ scraper.py
@@ -2,3 +2,14 @@ import requests
 import csv
 import time
 from bs4 import BeautifulSoup4
+
+# using the old reddit format and url since its easier to extract data
+url = "https://old.reddit.com/r/WasabiWallet/"
+# this mimics a browser visit
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+# gives us a requests.models.Response object - raw HTML for page
+page = requests.get(url, headers=headers)
+
+#beautifulsoup object 
+soup = BeautifulSoup(page.text, 'html.parser')
